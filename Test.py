7fc4b08d2c1d526f9d4120ea8cdfccb2dbf10344
Chaithanya Reddy <79004348+chaithanya99@users.noreply.github.com>
2022-11-25 12:34:13 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,16 +6,16 @@ from Factorial import factorial
 class TestSum(unittest.TestCase):
     def test_list_int(self):
         # Test case to add two numbers
-        a = 5
+        a = 15
         b = 10
         sum = add(a, b)
         
         # Test case to find factorial
-        c = 5
+        c = 6
         fact = factorial(c)
         
-        self.assertEqual(sum, 15)
-        self.assertEqual(fact, 140)
+        self.assertEqual(sum, 25)
+        self.assertEqual(fact, 720)
 
 if __name__ == '__main__':
     unittest.main()
